c022896f81aca0b542b911222b549ccf611dec01
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-04-20 07:31:59 -0500

    scroll extend

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,15 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste?', fromWho: FromWho.hers),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
